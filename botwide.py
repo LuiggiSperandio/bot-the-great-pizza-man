caeb9bf18fc15e44570225874f8953773f14b0f4
Luiggi Sperandio <luiggisperandio@gmail.com>
2024-08-21 17:55:47 -0300

    Added: crash loop

--- botwide.py
+++ botwide.py
@@ -22,43 +22,46 @@ pyautogui.PAUSE = 0
 
 # # returns (left, top, width, height) of matching region
 while True:
-    # if keyboard.is_pressed('q'):
-    #     currentMouseX, currentMouseY = pyautogui.position()
-    #     print(currentMouseX, currentMouseY)
-    #     sleep(1)
-    if keyboard.is_pressed('f1'):
-        currentMouseX, currentMouseY = pyautogui.position()
-        x, y = pyautogui.locateCenterOnScreen('qwide.png', confidence=0.35)
-        y=1021
-        print(x,y)
-        for index, item in enumerate(offset):
-            x1 = x + item[0]
-            y1 = y + item[1]
-            pyautogui.moveTo(x1,y1)
-            sleep(0.01)
-            pyautogui.click()
-        pyautogui.moveTo(currentMouseX, currentMouseY)
-    if keyboard.is_pressed('f2'):
-        currentMouseX, currentMouseY = pyautogui.position()
-        x, y = pyautogui.locateCenterOnScreen('qwide.png', confidence=0.35)
-        y=1021
-        print(x,y)
-        for index, item in enumerate(left_offset):
-            x1 = x + item[0]
-            y1 = y + item[1]
-            pyautogui.moveTo(x1,y1)
-            sleep(0.01)
-            pyautogui.click()
-        pyautogui.moveTo(currentMouseX, currentMouseY)
-    if keyboard.is_pressed('f3'):
-        currentMouseX, currentMouseY = pyautogui.position()
-        x, y = pyautogui.locateCenterOnScreen('qwide.png', confidence=0.35)
-        y=1021
-        print(x,y)
-        for index, item in enumerate(right_offset):
-            x1 = x + item[0]
-            y1 = y + item[1]
-            pyautogui.moveTo(x1,y1)
-            sleep(0.01)
-            pyautogui.click()
-        pyautogui.moveTo(currentMouseX, currentMouseY)
+    try:
+        # if keyboard.is_pressed('q'):
+        #     currentMouseX, currentMouseY = pyautogui.position()
+        #     print(currentMouseX, currentMouseY)
+        #     sleep(1)
+        if keyboard.is_pressed('f1'):
+            currentMouseX, currentMouseY = pyautogui.position()
+            x, y = pyautogui.locateCenterOnScreen('qwide.png', confidence=0.35)
+            y=1021
+            print(x,y)
+            for index, item in enumerate(offset):
+                x1 = x + item[0]
+                y1 = y + item[1]
+                pyautogui.moveTo(x1,y1)
+                sleep(0.01)
+                pyautogui.click()
+            pyautogui.moveTo(currentMouseX, currentMouseY)
+        if keyboard.is_pressed('f2'):
+            currentMouseX, currentMouseY = pyautogui.position()
+            x, y = pyautogui.locateCenterOnScreen('qwide.png', confidence=0.35)
+            y=1021
+            print(x,y)
+            for index, item in enumerate(left_offset):
+                x1 = x + item[0]
+                y1 = y + item[1]
+                pyautogui.moveTo(x1,y1)
+                sleep(0.01)
+                pyautogui.click()
+            pyautogui.moveTo(currentMouseX, currentMouseY)
+        if keyboard.is_pressed('f3'):
+            currentMouseX, currentMouseY = pyautogui.position()
+            x, y = pyautogui.locateCenterOnScreen('qwide.png', confidence=0.35)
+            y=1021
+            print(x,y)
+            for index, item in enumerate(right_offset):
+                x1 = x + item[0]
+                y1 = y + item[1]
+                pyautogui.moveTo(x1,y1)
+                sleep(0.01)
+                pyautogui.click()
+            pyautogui.moveTo(currentMouseX, currentMouseY)
+    except:
+        print('Except')
